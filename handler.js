5a584837729f9bfa7179a521779abf0420901c58
LuisPa <diganluispa@gmail.com>
2020-04-26 16:37:25 -0600

    setup

---
+++ handler.js
@@ -0,0 +1,10 @@
+export function main(event, context, callback) {
+	const happyDay = () => 'Friday';
+	const buildColor = 'green';
+	const goodNews = [ `Today is ${happyDay()}`, `Build is ${buildColor}` ];
+
+	callback(null, {
+		statusCode: 200,
+		body: JSON.stringify(goodNews)
+	});
+}
